0d9508138da5cc7ee4e4fb2aa1a4ac00540b5724
tomaszbilka <bila4@op.pl>
2023-06-13 16:38:12 +0200

    add useActionData and useLoaderData

--- app/routes/notes.jsx
+++ app/routes/notes.jsx
@@ -1,8 +1,8 @@
-import { redirect } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
-import NewNote, { links as newNotesLinks } from "~/components/NewNote";
-import NoteList, { links as noteListLinks } from "~/components/NoteList";
-import { getStoredNotes, storeNotes } from "~/data/notes";
+import { redirect } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
+import NewNote, { links as newNotesLinks } from '~/components/NewNote';
+import NoteList, { links as noteListLinks } from '~/components/NoteList';
+import { getStoredNotes, storeNotes } from '~/data/notes';
 
 const NotesPage = () => {
   const notes = useLoaderData();
@@ -23,11 +23,15 @@ export const action = async ({ request }) => {
   const formData = await request.formData();
   const noteData = Object.fromEntries(formData);
   //Add validation...
+  if (noteData.title.trim().length < 3) {
+    return { message: 'Invalid title - must be at least 3 characters long!' };
+  }
+
   const existingNotes = await getStoredNotes();
   noteData.id = new Date().toISOString();
   const updatedNotes = existingNotes.concat(noteData);
   await storeNotes(updatedNotes);
-  return redirect("/notes");
+  return redirect('/notes');
 };
 
 export const loader = async () => {
